c2cc43925af59c2121acce313ef27f4a05ce45c1
bj9851277 <dlqudwn0424@gmail.com>
2020-11-12 23:18:28 +0900

    ch06_02 (1112)

---
+++ src/com/gomddu/ch06/TvTest.java
@@ -0,0 +1,28 @@
+package com.gomddu.ch06;
+
+public class TvTest {
+    public static void main(String[] args) {
+        Tv t;
+        t = new Tv();
+        t.channel = 7;
+        t.channelDown();
+        System.out.println("현재 채널은 " +t.channel+ "입니다.");
+    }
+}
+
+class Tv {
+    // Tv의 속성
+    String color;
+    boolean power;
+    int channel;
+
+    void power() {
+        power = !power;
+    }
+    void channelUp() {
+        ++channel;
+    }
+    void channelDown() {
+        --channel;
+    }
+}
